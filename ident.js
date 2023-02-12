0fbf0b00584a9368898ec0ee3f120d812799cf9e
transformedharem <124980664+transformedharem@users.noreply.github.com>
2023-02-11 17:58:53 -0800

    Update ident.js

--- ident.js
+++ ident.js
@@ -1,21 +1,23 @@
 
 function codeAddress() {
 if ((/Android/i.test(navigator.userAgent))) {
+          location.replace("https://www.royalroad.com/fiction/63864/")
           alert('android');
 
     // go to Google Play Store
 
 }
 if (/iPad|iPhone|iPod/i.test(navigator.userAgent)) {
+          location.replace("https://www.royalroad.com/fiction/63864/")
           alert('apple');
 
-<!-- 	location.replace("https://www.w3schools.com")-->
+//<!-- 	location.replace("https://www.w3schools.com")-->
     // go to App Store
 
 }
 else {
-  alert('pc');
-<!--location.replace("https://www.google.com")-->
+ // alert('pc');
+ // location.replace("https://www.google.com")-->
     // go to another website
 }        }
 window.onload = codeAddress;
